af14138c4316146ca513b6985d31db18e9ff9515
Anmol Shubham <anmolshubhamsteam69@gmail.com>
2023-02-08 23:10:52 +0530

    Lab Component

---
+++ CreateanddisplayArray.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+int a[20];
+int n,val,i,pos;
+void create()
+{
+printf("\nEnter the size of the array elements:\t");
+scanf("%d",&n);
+printf("\nEnter the elements for the array:\n");
+for (int i=0; i<n; i++)
+{
+    scanf("%d",&a[i]);
+}
+}
+void display()
+{
+int i;
+printf("\nThe array elements are :\n");
+for(i=0;i<n;i++)
+{
+printf("%d\t",a[i]);
+}
+}
+int main()
+{
+int choice=1;
+while(choice)
+{
+printf("\n\n--------MENU-----------\n");
+printf("1.CREATE\n");
+printf("2.DISPLAY\n");
+printf("3.EXIT\n");
+scanf("%d",&choice);
+switch(choice)
+{
+case 1:
+create();
+break;
+case 2:
+display();
+break;
+case 3:
+exit(0);
+default:
+printf("\nInvalid choice:\n");
+}
+}
+return 0;
+}
