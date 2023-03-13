12f1dc2cb884d5a75e6247871055fc58c22ded81
Anmol Shubham <124705792+AnmolShubhamsteam@users.noreply.github.com>
2023-03-14 03:03:03 +0530

    Lab Component

---
+++ Hashmap.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TABLE_SIZE 10
+typedef struct {
+    int key;
+    int value;
+} pair;
+pair table[TABLE_SIZE];
+
+int hash(int key) {
+    return key % TABLE_SIZE;
+}
+void insert(int key, int value) {
+    int index = hash(key);
+    while (table[index].key != -1) {
+        index = (index + 1) % TABLE_SIZE;
+    }
+    table[index].key = key;
+    table[index].value = value;
+}
+int search(int key) {
+    int index = hash(key);
+    while (table[index].key != -1) {
+        if (table[index].key == key) {
+            return table[index].value;
+        }
+        index = (index + 1) % TABLE_SIZE;
+    }
+    return 0;
+}
+void print_table() {
+    int i;
+    printf("Key\tValue\n");
+    for (i = 0; i < TABLE_SIZE; i++) {
+        printf("%d\t%d\n", table[i].key, table[i].value);
+    }
+}
+int main() {
+    int key, value,n;
+    char ch;
+    printf("Enter no of students \n");
+    scanf("%d",&n);
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
+    table[i].key = -1;
+    table[i].value = 0;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter key and value to insert: ");
+        scanf("%d %d", &key, &value);
+        insert(key, value);
+    }
+    
+    printf("\nHash table contents:\n");
+    print_table();
+    printf("\nEnter key to search for: ");
+    scanf("%d", &key);
+    value = search(key);
+    if (value != 0) {
+        printf("Value found: %d\n", value);
+    } else {
+        printf("Key not found\n");
+    }
+    return 0;
+}
